a39df2a99aa4b8f4802f9ff3c3f24dcc9787fe98
asabogal <asabogal.git@gmail.com>
2019-05-08 20:15:41 -0400

    SEO ready with react-helmet + babel-polyfill

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Helmet from 'react-helmet'
+import 'babel-polyfill'
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      KON-TIKI
+      <Helmet>
+        <title>Kon-Tiki Greenport</title>
+        <meta name="description" content="Kon-Tiki Restaurant and Bar at The Gallery Hotel" />
+        <meta name="keywords" cpntent="greenport, restaurant, bar, gallery hotel, gallery, hotel" />
+      </Helmet>
+
     </div>
+    
   );
 }
 
