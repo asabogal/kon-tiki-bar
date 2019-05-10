a4d02c973a39d580d803d18f7096ac303c549dee
asabogal <asabogal.git@gmail.com>
2019-05-10 16:33:40 -0400

    add home component folder to hold homepage related components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Navbar from '../components/layout/Navbar';
-import LandingImage from '../components/layout/LandingImage'
+import LandingImage from '../components/home/LandingImage'
 
 class HomePage extends Component {
   render() {
     return (
      <React.Fragment>
-        <Navbar/>
         <LandingImage/>
      </React.Fragment> 
     );
